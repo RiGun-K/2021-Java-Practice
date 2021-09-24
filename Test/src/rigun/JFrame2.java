965c677137461f36eec9d74527f1ae5d88940110
RiGun-K <1234suho@naver.com>
2021-09-24 11:41:41 +0900

    2021/09/24

--- Test/src/rigun/JFrame2.java
+++ Test/src/rigun/JFrame2.java
@@ -1,6 +1,9 @@
 package rigun;
 
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -10,6 +13,16 @@ class MyFrame extends JFrame {
 		setSize(300,200);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("홍길동");
+		
+		// 아이콘 설정
+		Toolkit kit = Toolkit.getDefaultToolkit();
+		Image img = kit.getImage("c:\\dd.jpg");
+		this.setIconImage(img);
+		
+		// 프레임의 위치를 선정
+			// setLocation(200,100);
+		Dimension screenSize = kit.getScreenSize();
+		setLocation(screenSize.width/2, screenSize.height/2);
 		
 		JButton button1 = new JButton("확인");
 		// 컨테이너에 컴포넌트를 추가하기 위해 add 메소드를 추가한다.
