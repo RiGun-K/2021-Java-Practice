31fa7b3c34f5a2d1e9299a419d1e68756066b1bb
RiGun-K <1234suho@naver.com>
2021-09-27 11:40:11 +0900

    2021/09/27

--- Test/src/practice/Practice.java
+++ Test/src/practice/Practice.java
@@ -5,43 +5,43 @@ public class Practice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		// 자료형(타입) 변수명
-		// 첫글자는 소문자 , 두단어 이상은 이후 첫글자에 대문자
+		// �옄猷뚰삎(���엯) 蹂��닔紐�
+		// 泥リ��옄�뒗 �냼臾몄옄 , �몢�떒�뼱 �씠�긽�� �씠�썑 泥リ��옄�뿉 ��臾몄옄
 		int count = 10;
 		
 		count = 20;
-		// count = 11.1;  ( X - 자료형 double로 작성 )
+		// count = 11.1;  ( X - �옄猷뚰삎 double濡� �옉�꽦 )
 		System.out.print(count);
 		
-		// 하나의 정해진 값은 상수를 사용하면 편리하다 
+		// �븯�굹�쓽 �젙�빐吏� 媛믪� �긽�닔瑜� �궗�슜�븯硫� �렪由ы븯�떎 
 		final int PRICE = 8920;
 		int time = 5 * PRICE;
-		System.out.println("오늘 내가 일한 일당은" + time + "입니다.");
+		System.out.println("�삤�뒛 �궡媛� �씪�븳 �씪�떦��" + time + "�엯�땲�떎.");
 	
 	    ///////////////////////////////////////////////////////
 		
 		/*
-		 * 자료형 타입
+		 * �옄猷뚰삎 ���엯
 		 * 
-		 *  char ( 2byte , 한 글자만 가능 )
+		 *  char ( 2byte , �븳 湲��옄留� 媛��뒫 )
 		 *  -> byte -> short -> char -> int -> long
 		 *  
-		 *  실수형
+		 *  �떎�닔�삎
 		 *  float -> double 
-		 *  단, float 는 값 뒤에 f를 붙여야 함 ! 
-		 *  = float f = 32.4f;
+		 *  �떒, float �뒗 媛� �뮘�뿉 f瑜� 遺숈뿬�빞 �븿 ! 
+		 *  = float f = 32.4f;;
 		 */
 		
 		int x = 50000;
 		long y = x;
-		/* 묵시적 형변환
-		 * 작은놈 (int) => 큰놈 (long) 형변환 ( O )
-		 * 큰놈 => 작은놈 형변환 ( X ) 
+		/* 臾듭떆�쟻 �삎蹂��솚
+		 * �옉���냸 (int) => �겙�냸 (long) �삎蹂��솚 ( O )
+		 * �겙�냸 => �옉���냸 �삎蹂��솚 ( X ) 
 		 */
 		long x2 = 5;
 //		int y2 = x2;
 		int y2 = (int) x2;
-		// 명시적 형변환 = 타입을 ( ) 붙여주면 가능 ! 
+		// 紐낆떆�쟻 �삎蹂��솚 = ���엯�쓣 ( ) 遺숈뿬二쇰㈃ 媛��뒫 ! 
 		
 	}
 
