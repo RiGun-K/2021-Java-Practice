125af81c159dfb4fdf8f2d846695dcd00920469c
RiGun-K <1234suho@naver.com>
2021-10-16 19:50:48 +0900

    2021/10/16

--- Test/src/rigun/Practice_1016_05.java
+++ Test/src/rigun/Practice_1016_05.java
@@ -1,5 +1,6 @@
 package rigun;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Panel;
 
@@ -10,9 +11,13 @@ class MyPanel1016_05 extends JPanel {
 	// 컴포넌트에 그리는 메소드 , x는 → y는 ↓ 갈수록 증가 
 	public void paintComponent(Graphics g) {
 		super.paintComponents(g);
+		g.setColor(Color.blue);
 		g.drawString("안녕하세요", 10, 10);
 		g.drawLine(10, 20, 110, 20);
+		// 여기서부터 빨강색 
+		g.setColor(Color.red);
 		g.drawRect(10, 30, 100, 100);
+//		g.fillOval(20, 30, 200, 200); = 타원생성 
 	}
 }
 class MyFrame1016_05 extends JFrame {
