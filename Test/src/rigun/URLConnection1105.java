8a8f244db2725b45280fc90f8d846ed25a81c842
RiGun-K <1234suho@naver.com>
2021-11-05 11:41:52 +0900

    2021/11/05

---
+++ Test/src/rigun/URLConnection1105.java
@@ -0,0 +1,47 @@
+package rigun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+// URL 객체를 이용하여 해당 주소의 데이터를 읽어온다 ! 
+
+public class URLConnection1105 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		URL naver = null;
+		try {
+			naver = new URL("https://www.naver.com/");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		try {
+			URLConnection nc = naver.openConnection();
+			InputStream is = nc.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br= new BufferedReader(isr);
+			
+			String data;
+			
+			while( (data = br.readLine()) != null) {
+				System.out.println(data);
+			}
+			br.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
