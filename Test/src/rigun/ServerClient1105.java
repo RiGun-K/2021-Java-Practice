0c8562e7c74393f6e2068c5ba5799dc6d02bee40
RiGun-K <1234suho@naver.com>
2021-11-05 12:06:50 +0900

    11/04

---
+++ Test/src/rigun/ServerClient1105.java
@@ -0,0 +1,16 @@
+package rigun;
+
+// 서버에 접속하여 자신의 이름을 보낸다.
+/*
+ * 1. 소켓 객체를 생성
+ * 2. 해당 객체로 부터 서버의 데이터를 보내기(출력)위한 PrintWriter 객체를 생성
+ * 3.  
+ */
+public class ServerClient1105 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
