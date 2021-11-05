018ecaaa07ce8a8547e9539dfdb4205b664babdd
RiGun-K <1234suho@naver.com>
2021-11-05 12:07:41 +0900

    11/04

--- Test/src/rigun/ServerClient1105.java
+++ Test/src/rigun/ServerClient1105.java
@@ -4,7 +4,8 @@ package rigun;
 /*
  * 1. 소켓 객체를 생성
  * 2. 해당 객체로 부터 서버의 데이터를 보내기(출력)위한 PrintWriter 객체를 생성
- * 3.  
+ * 3. PrintWriter 객체에 이름을 보낸다
+ * 4. 소켓을 닫는다.
  */
 public class ServerClient1105 {
 
